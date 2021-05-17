fba378ed98f8bb704fc3037c0dfaa0eb6147787f
Laura Moon <21319380+lauramoon@users.noreply.github.com>
2021-05-17 11:48:59 -0500

    complete curried-add

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,21 @@
 function curriedAdd(total) {
+  let sumTotal = 0;
 
+  function curriedAddRecursive(total) {
+    if (arguments.length === 0) {
+      return sumTotal;
+    } else {
+      sumTotal += total;
+      return curriedAddRecursive;
+    }
+  }
+
+  if (arguments.length === 0) {
+    return 0;
+  } else {
+    sumTotal = total;
+    return curriedAddRecursive;
+  }
 }
 
 module.exports = { curriedAdd };
